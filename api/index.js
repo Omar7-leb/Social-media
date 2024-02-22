e29a0a6135897b57e20f62677a9703304a870511
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2024-02-22 20:01:47 +0200

    add sharepost functionality and upload image

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ import authRoutes from "./routes/auth.js";
 import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
+import multer from "multer";
 
 const app = express();
 
@@ -19,6 +20,22 @@ app.use(cors({
     origin :"http://localhost:3000",
 }));
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage });
+
+  app.post("/api/upload", upload.single("file"),(req,res)=>{
+    const file = req.file;
+    res.status(200).json(file.filename)
+  })
 
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
